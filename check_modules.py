1dee43b9d7858a3b389f62ba685978a0b07ffd38
wheaton5 <whheaton@gmail.com>
2019-08-01 23:24:43 +0100

    little thing to check module loading

---
+++ check_modules.py
@@ -0,0 +1,14 @@
+import numpy as np
+import tensorflow as tf
+import scipy
+import gzip
+import math
+import pystan
+import vcf
+import pysam
+import pyfasta
+import subprocess
+import time
+import os
+
+print("check successful"_)
